ce261480bdad2ecf5694e5f01092d10f3cf1bc61
Muhammad <muhannmmad@gmail.com>
2024-09-19 15:15:19 +0200

    coded

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -1,9 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show BuildContext, Center, Key, StatelessWidget, Text, TextStyle, Widget;
 
 class S4521 extends StatelessWidget {
   const S4521({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
   }
 }
