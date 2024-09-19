31c976dfe5f7ac2dda3ac56b1e5840bbb850ac1e
Muhammad <muhannmmad@gmail.com>
2024-09-19 15:15:33 +0200

    coded

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 
 class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Hello Muhammad',
+            style: TextStyle(fontSize: 24),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              print('Button Pressed!');
+            },
+            child: const Text('Press Me'),
+          ),
+        ],
+      ),
+    );
   }
 }
